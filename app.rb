e42a3aa040481162076e05fc46b96ade4c6774f4
Ryan Cook <ryancook@ryans-mbp.home>
2020-03-26 11:05:36 +1100

    Gems added, general overview in pseudo

--- app.rb
+++ app.rb
@@ -1,6 +1,35 @@
 # Gems
+require "tty-prompt"
 require "timers"
 timers = Timers::Group.new
+
+=begin========== TIME PERMITTING ===============
+Add our term apps as ruby gems.
+Implement graphics
+================================================
+=end
+=begin Cutie-pie, Relaxed, Stern ~~ Variables for each. Potentially we could create A personality/partner Class and create 3 instances of that.
+Get name from User.
+Choose a personality type
+Greeting screen/Intro to program "Hey lover type something"
+Run down of app and what it does for you "Hi, I'm your AI partner, what would you like to..."
+Present user with menu of what they'd like to do. TTY-Prompt.
+--------- OPTIONS ------------
+Prompt for how frequently you want to be reminded to do these things?
+Drink water -- These are just on timers
+Leave me alone -- Timers
+Take a break
+    - how long you want to take for
+Stretch
+    - Take through routine
+Hour mark, play a game (OUR OWN TERM APPS)
+    - Our ruby gems
+Symptom checker for the Vid.
+    - runs through symptoms list, answer each y/n
+Flirting men for the el oh els. Pickup line generator/positive reinforcement ~~ PERSONALITY DEPENDENT ~~
+    - Randomised se
+=end
+
 
 
 
